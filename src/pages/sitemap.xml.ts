317570787a85da6fbf9fd8e3cbb11ddc2197fe6f
Codeelchi <timmygleixner@gmail.com>
2026-08-14 06:52:40 +0200

    seo: include link hub in sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -6,7 +6,7 @@ const escapeXml = (value: string) => value.replace(/[<>&'\"]/g, (char) => ({ '<'
 
 export const GET: APIRoute = async () => {
   const shorts = (await getCollection('shorts')).filter((entry) => !entry.data.demo);
-  const routes = ['', 'shorts/', 'topics/', 'about/', 'imprint/', 'privacy/', ...shorts.map((entry) => `shorts/${entry.data.slug}/`)];
+  const routes = ['', 'shorts/', 'topics/', 'about/', 'links/', 'imprint/', 'privacy/', ...shorts.map((entry) => `shorts/${entry.data.slug}/`)];
   const urls = routes.map((route) => `<url><loc>${escapeXml(absoluteUrl(route))}</loc></url>`).join('');
   return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
 };
